d647c38a44e464c67bd8c781c8f4e725d20fbb2b
Abdirahman Hashi <hashi156@umn.edu>
2025-10-21 20:02:13 +0000

    Initial commit of PHP Crude CRUD Application

---
+++ credentials.php
@@ -0,0 +1,8 @@
+<?php
+//remeto populate the data here
+//
+$servername = 'localhost';     //if apache on diff server from db or using Docker, set IP address
+$dbname = '';              //which db you're going to use
+$username = '';
+$password = '';
+?>
